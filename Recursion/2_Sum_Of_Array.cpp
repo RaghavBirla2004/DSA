686159c8c2812cf71798a9de6330ba33126b96a2
Raghav Birla <71842917+RaghavBirla2004@users.noreply.github.com>
2024-06-01 22:15:01 +0530

    Create 2_Sum_Of_Array.cpp

---
+++ Recursion/2_Sum_Of_Array.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+	// function to return sum of elements
+	// in an array of size n
+	int sum(int arr[], int n) {
+	    // code here
+	   
+	    if(n==0){
+	        return 0;
+	    }
+	    else{
+	        return arr[n-1] + sum(arr,n-1);
+	    }
+	}
+};
