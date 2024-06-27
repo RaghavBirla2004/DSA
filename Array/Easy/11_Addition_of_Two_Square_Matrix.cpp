aa805cdeb1f1c9c593c221cd4fbb2a9daead8a89
Raghav Birla <71842917+RaghavBirla2004@users.noreply.github.com>
2024-06-27 21:44:42 +0530

    Create 11_Addition_of_Two_Square_Matrix.cpp

---
+++ Array/Easy/11_Addition_of_Two_Square_Matrix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void Addition(vector<vector<int> >& matrixA, vector<vector<int>>&matrixB) {
+        // Code here
+        for(int i=0;i<matrixA.size();i++){
+            for(int j=0;j<matrixA[0].size();j++){
+                matrixA[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+    }
+};
