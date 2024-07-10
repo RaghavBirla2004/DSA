ee3c82d0325121eda08729c7c0f6727be8030ad5
Raghav Birla <71842917+RaghavBirla2004@users.noreply.github.com>
2024-07-10 21:19:23 +0530

    Create 2_Crawler_Log_Folder.cpp

---
+++ Basic/2_Crawler_Log_Folder.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int count = 0;
+     for(int i=0;i<logs.size();i++){
+        if(logs[i]=="./"){
+            
+        }
+        else if(logs[i]=="../"){
+            if (count > 0) {
+                    count--; 
+        }
+        }
+        else {
+            count++;
+        }
+        
+     }   
+     return count;
+    }
+};
