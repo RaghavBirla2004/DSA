377ab89e94c24e002647a6bd8b80d5ebae5b2ddb
Raghav Birla <71842917+RaghavBirla2004@users.noreply.github.com>
2024-07-11 22:18:10 +0530

    Create 3_Reverse_SubStrings_Between_each_pair_of_parentheses.cpp

---
+++ Stack/Medium/3_Reverse_SubStrings_Between_each_pair_of_parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int>match;
+        int n = s.size();
+        for(int i=0;i<s.length();i++){
+          if(s[i]=='('){
+          match.push(i);
+             }
+             else if(s[i]==')'){
+                int start = match.top()+1;
+                int end = i;
+                reverse(s.begin()+ start, s.begin()+end);
+                match.pop();
+             }
+      
+        }
+        string ans = "";
+        for(int i=0;i<n;i++){
+          if(s[i]=='(' || s[i]==')'){
+            continue;
+          }
+          else{
+            ans+=s[i];
+          }
+        }
+        return ans;
+    }
+};
